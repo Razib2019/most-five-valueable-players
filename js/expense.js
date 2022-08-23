5ad3f3cfc1f4be81509384b375cadcf1fe71f691
Razib2019 <hafizurrahmanrazib@gmail.com>
2022-08-23 21:22:25 +0600

    function added for not repeat same code

--- js/expense.js
+++ js/expense.js
@@ -1,35 +1,24 @@
 document.getElementById('btn-calculate').addEventListener('click', function () {
-    const perPlayerExpenseField = document.getElementById('player-expense-field');
-    const perPlayerExpenseString = perPlayerExpenseField.value;
-    const perPlayerExpense = parseFloat(perPlayerExpenseString);
-    perPlayerExpenseField.value = '';
 
-    const NumberOfThePlayer = document.getElementById('total-added-player');
-    const numberOfThePlayerString = NumberOfThePlayer.innerText;
-    const numberOfTotalPlayer = parseInt(numberOfThePlayerString);
+    const perPlayerExpense = getInputFieldValueById('player-expense-field');
+
+    const numberOfTotalPlayer = getTextElementValueById('total-added-player');
 
     const totalPlayerExpense = perPlayerExpense * numberOfTotalPlayer;
 
-    const totalPlayerExpenses = document.getElementById('players-total-expenses');
-    totalPlayerExpenses.innerText = totalPlayerExpense;
+    setTextElementValueById('players-total-expenses', totalPlayerExpense);
 
 });
 
 document.getElementById('btn-calculate-total').addEventListener('click', function () {
-    const managerExpenseField = document.getElementById('manager-expense-field');
-    const managerExpenseString = managerExpenseField.value;
-    const managerExpense = parseFloat(managerExpenseString);
 
-    const coachExpenseField = document.getElementById('coach-expense-field');
-    const coachExpenseString = coachExpenseField.value;
-    const coachExpense = parseFloat(coachExpenseString);
+    const managerExpense = getInputFieldValueById('manager-expense-field');
 
-    const totalPlayerExpenses = document.getElementById('players-total-expenses');
-    const totalPlayerExpensesString = totalPlayerExpenses.innerText;
-    const totalPlayerExpenseAmount = parseFloat(totalPlayerExpensesString);
+    const coachExpense = getInputFieldValueById('coach-expense-field');
+
+    const totalPlayerExpenseAmount = getTextElementValueById('players-total-expenses');
 
     const total = totalPlayerExpenseAmount + managerExpense + coachExpense;
 
-    const calculateTotalExpense = document.getElementById('calculate-total-expense');
-    calculateTotalExpense.innerText = total;
+    setTextElementValueById('calculate-total-expense', total);
 });
