882c98c1db1ff33c99bce582041de9fe9751c111
Razib2019 <hafizurrahmanrazib@gmail.com>
2022-08-23 21:47:51 +0600

    input field validation done

--- js/expense.js
+++ js/expense.js
@@ -2,6 +2,11 @@ document.getElementById('btn-calculate').addEventListener('click', function () {
 
     const perPlayerExpense = getInputFieldValueById('player-expense-field');
 
+    if (isNaN(perPlayerExpense)) {
+        alert('Please enter a valid amount and make sure its not text or minus number')
+        return '';
+    }
+
     const numberOfTotalPlayer = getTextElementValueById('total-added-player');
 
     const totalPlayerExpense = perPlayerExpense * numberOfTotalPlayer;
@@ -15,6 +20,11 @@ document.getElementById('btn-calculate-total').addEventListener('click', functio
     const managerExpense = getInputFieldValueById('manager-expense-field');
 
     const coachExpense = getInputFieldValueById('coach-expense-field');
+
+    if (isNaN(managerExpense) || isNaN(coachExpense)) {
+        alert('Please enter a valid amount and make sure its not text or minus number');
+        return '';
+    }
 
     const totalPlayerExpenseAmount = getTextElementValueById('players-total-expenses');
 
