dc4e1abff7a16e81e00e0f5c8826d6008a08c093
Razib2019 <hafizurrahmanrazib@gmail.com>
2022-08-23 19:12:50 +0600

    player list added

---
+++ js/players.js
@@ -0,0 +1,27 @@
+const playerlist = [];
+
+function display(players) {
+    const listContainer = document.getElementById('selceted-player');
+    listContainer.innerText = '';
+    for (i = 0; i < players.length; i++) {
+        if (i === 5) {
+            alert('You Can not add more than 5 player');
+            return;
+        }
+        const name = playerlist[i];
+
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+        <th>${i + 1}.</th>
+        <td>${name}</td>
+        `;
+        listContainer.appendChild(tr);
+    }
+}
+
+function addToSelectedPlayers(element) {
+    const playerName = element.parentNode.children[1].innerText;
+    playerlist.push(playerName);
+    document.getElementById('total-added-player').innerText = playerlist.length;
+    display(playerlist);
+}
